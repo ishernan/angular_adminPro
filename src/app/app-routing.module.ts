5fa385bd01df515f1908f6f13bf2ca0eb8ab5757
HERNANDEZ AGUIRRE <isaac.hernandez-aguirre@capgemini.com>
2022-04-01 10:18:42 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthRoutingModule } from './auth/auth.routing';
 
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { PagesRoutingModule } from './pages/pages.routing';
 
-const routes: Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
-  
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+const routes: Routes = [  
 
+  //COMANTARIO PARA INDICAR QUE RUTAS ESTAN TOMADAS Y DONDE ESTAN TRABAJANDO
+  // path: '/dashboard', PagesRouting
+  // path: '/auth', AuthRouting
+  // path: '/medicos', AuthRouting
+  // path: '/compras', ComprasRouting
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent },
 ];
-
-
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule, //se incluye la ruta hija
+    AuthRoutingModule
   ],
   exports: [ RouterModule ]
 })
